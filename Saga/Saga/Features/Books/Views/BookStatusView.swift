9b97a14f4bb2d44b2007f06854d3e8a2a162bcc3
Dylan Gattey <dylan.gattey@gmail.com>
2025-08-20 21:12:49 -0700

    changing animations + styles of status view

--- Saga/Saga/Features/Books/Views/BookStatusView.swift
+++ Saga/Saga/Features/Books/Views/BookStatusView.swift
@@ -12,18 +12,28 @@ struct BookStatusView: View {
     var book: Book
     
     var body: some View {
-        if let readDateFinished = book.readDateFinished {
-            HStack(alignment: .center, spacing: 4) {
-                Image(systemName: "checkmark.seal.fill")
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(Color.accentForeground, Color.accentColor)
-                    .imageScale(.small)
-                Text("Finished \(readDateFinished.formatted(date: .abbreviated, time: .omitted))").font(.caption)
-            }
+        if book.readDateFinished != nil {
+            // Already read
+            EmptyView()
         } else if book.readDateStarted != nil {
-            Text("Reading").font(.caption)
+            // Currently reading
+            HStack(alignment: .center, spacing: 4) {
+                Image(systemName: "book.fill")
+                Text("Reading")
+            }
+            .font(.caption)
+            .padding(.vertical, 4)
+            .symbolRenderingMode(.hierarchical)
+            .symbolEffect(.bounce, options: .nonRepeating)
+            .background {
+                RoundedRectangle(cornerRadius: 4)
+                    .foregroundStyle(.accent.mix(with: .primary, by: 0.2).opacity(0.5))
+                    .defaultShadow()
+                    .padding(.horizontal, -4)
+            }
         } else if book.createdAt != nil {
-            Text("Up next").font(.caption)
+            // Up next
+            EmptyView()
         }
     }
 }
--- Saga/Saga/Shared/Views/FileDropZoneContainer.swift
+++ Saga/Saga/Shared/Views/FileDropZoneContainer.swift
@@ -41,9 +41,10 @@ struct FileDropZoneContainer<Content: View>: View {
                     Image(systemName: "document.badge.arrow.up")
                         .font(.system(size: 48))
                         .foregroundStyle(.accent)
+                        .symbolRenderingMode(.hierarchical)
                         .symbolEffect(
                             .bounce.down.byLayer,
-                            options: .repeat(.periodic(delay: 0.5))
+                            options: .repeat(.periodic(delay: 0.75))
                         )
                     Text("Drop file to upload")
                 }
